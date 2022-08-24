f15a2fab49c9ad17d66eb48f290ed749b33ffaf3
xiyang <90125263+JBossBC@users.noreply.github.com>
2022-08-24 23:39:33 +0800

    additional decorating mode

--- Design Mode/src/decorating_mode.go
+++ Design Mode/src/decorating_mode.go
@@ -1,10 +1,49 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+// decorating moded 能够无限套娃，这个基类的成员变量的作用非常关键***
+func main() {
+	pizza := &VeggieMania{}
+	pizzaWithCheese := &CheeseTopping{
+		pizza: pizza,
+	}
+	pizzaWithCheeseAndTomato := &TomatoTopping{
+		pizza: pizzaWithCheese,
+	}
+	fmt.Printf("Price of veggeMania with tomato and cheese topping is %d\n", pizzaWithCheeseAndTomato.getPrice())
 }
 
 //零件接口
 type IPizza interface {
 	getPrice() int
+}
+
+// 具体零件
+type VeggieMania struct {
+}
+
+func (p *VeggieMania) getPrice() int {
+	return 15
+}
+
+// 具体装饰
+type TomatoTopping struct {
+	pizza IPizza
+}
+
+func (c *TomatoTopping) getPrice() int {
+	pizzaPrice := c.pizza.getPrice()
+	return pizzaPrice + 7
+}
+
+type CheeseTopping struct {
+	pizza IPizza
+}
+
+func (c *CheeseTopping) getPrice() int {
+	pizzaPrice := c.pizza.getPrice()
+	return pizzaPrice + 10
 }
